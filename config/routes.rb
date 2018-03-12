60b2377dbd945d66160ea080db1abe01a577d1f1
Vastas96 <dukatekas@gmail.com>
2018-03-12 23:49:43 +0200

    fixed uploading file

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'games/file'
+
   resources :games
   resources :players
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
