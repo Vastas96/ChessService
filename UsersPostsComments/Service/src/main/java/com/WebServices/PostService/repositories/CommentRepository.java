05669f1c1cee47e4c86b9d66daa7765313d534ef
root <root@ubuntu.cloud>
2018-04-12 11:07:07 +0300

    Remove microservice from master (WIP in branch comments)

--- UsersPostsComments/Service/src/main/java/com/WebServices/PostService/repositories/CommentRepository.java
+++
@@ -1,7 +0,0 @@
-package com.WebServices.PostService.repositories;
-
-import com.WebServices.PostService.models.Comment;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Long>{
-}
